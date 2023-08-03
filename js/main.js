d21d2e0805104fa2a74192804a49688f25971e8e
Sgeary88 <97266345+Sgeary88@users.noreply.github.com>
2023-08-03 16:51:33 -0700

    got toggle theme to work

--- js/main.js
+++ js/main.js
@@ -1,10 +1,64 @@
+const theme = 'theme';
+const dataTheme = 'data-theme';
+const themeTab = '.theme-tab';
+const switcherBtn = '.switcher-btn';
+const dark = 'dark';
+const light = 'light';
+const open = 'open';
+const active = 'active';
+
+
 const modalOpen = '[data-open]';
 const modalClose = '[data-close]';
 const isVisible = 'is-visible';
 
+const root = document.documentElement;
 
+/***************************** THEME *****************************/
+const toggleTheme = document.querySelector(themeTab);
+const switcher = document.querySelectorAll(switcherBtn);
+const currentTheme = localStorage.getItem(theme);
+
+
+/***************************** MODAL *****************************/
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
+
+const setActive = (element, selector) => {
+	if (document.querySelector(`${selector}.${active}`) !== null) {
+		document.querySelector(`${selector}.${active}`).classList.remove(active);
+	} else {
+		element.classList.add(active);
+	}
+};
+
+const setTheme = (val) => {
+	if (val === dark) {
+		root.setAttribute(dataTheme, dark);
+		localStorage.setItem(theme, dark);
+	} else {
+		root.setAttribute(dataTheme, light);
+		localStorage.setItem(theme, light);
+	}
+}
+
+toggleTheme.addEventListener('click', function() {
+	const tab = this.parentElement.parentElement;
+	if(!tab.className.includes(open)) {
+		tab.classList.add(open);
+	} else {
+		tab.classList.remove(open);
+	}
+});
+
+for(const element of switcher) {
+	element.addEventListener('click', function() {
+		const toggle = this.dataset.toggle;
+		// set active state
+		setActive(element, switcherBtn);
+		setTheme(toggle);
+	})
+}
 
 // For Site Modal 'open buttons'
 for (const element of openModal) {
@@ -14,6 +68,7 @@ for (const element of openModal) {
 	})
 }
 
+// For Site Modal 'close function'
 for (const element of closeModal) {
 	element.addEventListener('click', function() {
 		this.parentElement.parentElement.classList.remove(isVisible);
